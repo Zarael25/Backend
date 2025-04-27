f014df81597b15078f81920c3ddf265387da709b
Zarael25 <telecom25a@gmail.com>
2025-04-27 18:18:55 -0400

    Modelo Usuarios y Negocios: Se realizaron los Modelos ORM para cada tabla

--- fichas_backend/settings.py
+++ fichas_backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
+    'negocios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,12 +74,27 @@ WSGI_APPLICATION = 'fichas_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'FichaDB',                
+        'USER': 'postgres',                      
+        'PASSWORD': 'admin',               
+        'HOST': 'localhost',                        
+        'PORT': '5432',                             
     }
 }
+
+
+
+
+
 
 
 # Password validation
