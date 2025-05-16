9987f7ccceb31ef126a5303d549bf198bc5be96e
Zarael25 <telecom25a@gmail.com>
2025-05-16 01:17:57 -0400

    Mejoras y correcciones, agregada ip estática

--- fichas_backend/settings.py
+++ fichas_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gaabe4*q8e&hs%rji7(gi)w&)7%+h#%#fg=r6pq%r7lpfzimrp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2', '192.168.1.10']
 
 
 # Application definition
