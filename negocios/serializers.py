28b34543480e86b799526cc33799bf14d8558261
Zarael25 <telecom25a@gmail.com>
2025-05-19 22:34:45 -0400

    Registrar Negocio: Servicio para crear negocios para ususarios completado

--- negocios/serializers.py
+++ negocios/serializers.py
@@ -5,6 +5,7 @@ class NegocioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Negocio
         fields = '__all__'
+        read_only_fields = ['usuario']
 
 class AtencionSerializer(serializers.ModelSerializer):
     class Meta:
--- negocios/views.py
+++ negocios/views.py
@@ -9,14 +9,25 @@ from .serializers import NegocioSerializer, AtencionSerializer, TicketSerializer
 from .services import obtener_negocios_por_usuario
 
 class NegocioViewSet(viewsets.ModelViewSet):
-    queryset = Negocio.objects.all()
+    queryset = Negocio.objects.all() 
     serializer_class = NegocioSerializer
+    permission_classes = [IsAuthenticated]  # Protege todas las operaciones del ViewSet
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Negocio.objects.all()
+        return Negocio.objects.filter(usuario=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
 
     @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='mis_negocios')
     def mis_negocios(self, request):
         negocios = obtener_negocios_por_usuario(request.user)
         serializer = self.get_serializer(negocios, many=True)
         return Response(serializer.data)
+    
+
 
 
 
