573b8b3fad1d12bce9d774ebfa28bd52b0b20005
Zarael25 <telecom25a@gmail.com>
2025-05-25 11:15:20 -0400

    Añadida las variables de entorno

--- fichas_backend/settings.py
+++ fichas_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,15 +88,14 @@ WSGI_APPLICATION = 'fichas_backend.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'FichaDB',                
-        'USER': 'postgres',                      
-        'PASSWORD': 'admin',               
-        'HOST': 'localhost',                        
-        'PORT': '5432',                             
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 
 
 
