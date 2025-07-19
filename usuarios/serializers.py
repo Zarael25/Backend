ce31d6cce91a1cc1378159cef1152cf93dfdb42b
Zarael25 <telecom25a@gmail.com>
2025-07-19 15:32:10 -0400

    Se agrego nuevas configuraciones para mostrar cuanto tiempo tiene el usuario para cancelar un ticket

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Usuario, UsuarioTicket, LogUsuario
-
+from datetime import timedelta
+from django.utils import timezone
 class UsuarioSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)  # Para que no se exponga al leer
 
@@ -53,6 +54,9 @@ class UsuarioTicketDetalleSerializer(serializers.ModelSerializer):
     negocio_nombre = serializers.CharField(source='ticket.fila_atencion.negocio.nombre', read_only=True)
     permitir_cancelacion = serializers.BooleanField(source='ticket.fila_atencion.negocio.permite_cancelar', read_only=True)
 
+    # Campo calculado: hasta qué hora se puede cancelar el ticket
+    minutos_restantes_cancelacion = serializers.SerializerMethodField()
+
     class Meta:
         model = UsuarioTicket
         fields = [
@@ -60,8 +64,28 @@ class UsuarioTicketDetalleSerializer(serializers.ModelSerializer):
             'nombre', 'correo',
             'estado', 'fecha_hora_registro', 'fecha_hora_atencion', 'posicion',
             'fila_nombre', 'negocio_nombre','permitir_cancelacion',
+            'minutos_restantes_cancelacion',
 
         ]
+
+
+    def get_minutos_restantes_cancelacion(self, obj):
+        negocio = obj.ticket.fila_atencion.negocio
+        
+        if not negocio.permite_cancelar:
+            return 0  # no permite cancelar, 0 minutos restantes
+        
+        tiempo_limite = negocio.tiempo_limite_cancelacion or 0
+        fecha_limite = obj.ticket.fecha_hora_registro + timedelta(minutes=tiempo_limite)
+        ahora = timezone.now()
+
+        minutos_restantes = (fecha_limite - ahora).total_seconds() / 60
+
+        if minutos_restantes <= 0:
+            return 0
+        
+        return int(minutos_restantes)
+    
 
 
 class LogUsuarioSerializer(serializers.ModelSerializer):
